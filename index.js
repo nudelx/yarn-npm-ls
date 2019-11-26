7d53aa7065ffb6a59b599af470b267e782f88a09
nudelx <nudelx@gmail.com>
2019-11-26 10:58:39 +0200

    fix path and empty script

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 /* eslint-disable no-console */
 const path = require('path')
 const chalk = require('chalk')
-const ROOT_PATH = process.env.ROOT_PATH || path.resolve(__dirname, '.') // eslint-disable-line
-console.log("ROOT", ROOT_PATH)
+const ROOT_PATH = process.env.ROOT_PATH || path.resolve(__dirname, `../..`) // eslint-disable-line
 const list = require(`${ROOT_PATH}/package.json`)
 const exec = require('child_process').execSync
 console.log(exec('clear', { encoding: 'utf8' }))
+
+if (!list.scripts) {
+  console.log(chalk.blueBright(' 🤔  Your scripts are empty: \n'))
+  return
+}
 console.log(chalk.blueBright(' 👉  Available commands are: \n'))
 Object.keys(list.scripts).map(k => console.log(`${chalk.yellow('✷')} - ${chalk.greenBright(k)}`))
 console.log(' \n\n')
-
